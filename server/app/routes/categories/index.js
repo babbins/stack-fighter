0db135b8edc2c6892996e259b26606da74dccb52
Naohito Sadoshima <nsadoshima@gmail.com>
2016-09-13 17:33:48 -0400

    made linter happier

--- server/app/routes/categories/index.js
+++ server/app/routes/categories/index.js
@@ -5,32 +5,29 @@ module.exports = router;
 var models = require('../../../db/models');
 var Category = models.Category;
 
-
-
 //Get all Categories with a given type. Eg: Every value (eg: SFII,SFIII,SIV) of a given type (eg: Game);
-router.get('/', function(req,res,next){
+router.get('/', function(req, res, next){
     Category.findAll({ where: req.query })
     .then(gettingCategories => res.json(gettingCategories))
     .catch(next);
 });
 
-
 //Creating a new category.
-router.post('/', function(req,res,next){
+router.post('/', function(req, res, next){
     Category.create(req.body)
     .then(creatingCategory => res.status(201).json(creatingCategory))
     .catch(next);
 });
 
 //Updating a category.
-router.put('/:categoryId', function(req,res,next){
+router.put('/:categoryId', function(req, res, next){
     req.category.update(req.body)
     .then(updatedCategory => res.status(200).json(updatedCategory))
     .catch(next);
 });
 
 //Deleting a category.
-router.delete('/:categoryId', function(req,res,next){
+router.delete('/:categoryId', function(req, res, next){
     req.category.destroy()
     .then(() => res.status(204).end())
     .catch(next);
