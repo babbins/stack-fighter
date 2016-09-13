6defb1200deb30fcc067611d47aa195467b2e5a1
Matt Elmquist <mattelmquist@gmail.com>
2016-09-13 12:23:15 -0400

    corrected User-Review relationship and deleted lodash/crypto

--- server/db/index.js
+++ server/db/index.js
@@ -8,4 +8,4 @@ var User = require('./models/user');
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
 
-Review.belongsTo(User);
+User.hasMany(Review, {as: 'reviews'});
