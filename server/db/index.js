88353cd2a62038e149b9559eae73b0497fe919d9
Naohito Sadoshima <nsadoshima@gmail.com>
2016-09-13 11:16:36 -0400

    added associations on db index

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,10 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
+var Category = require('./models/category')
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
+
+
+Category.belongsToMany(Character, {through: CategoryCharacters})
