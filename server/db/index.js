9859f6d4bb0a92abe6a0b14df0fa7b0eef6f323b
Nikita Petukhov <nedoumok99@gmail.com>
2016-09-13 14:15:20 -0400

    further resolve

--- server/db/index.js
+++ server/db/index.js
@@ -4,12 +4,12 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
-<<<<<<< HEAD
+
 var Order = require('./models/order')
-=======
+
 var Character = require('./models/character');
 var Category = require('./models/category');
->>>>>>> master
+
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
