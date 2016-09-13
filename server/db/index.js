46986c1beefa1fe5aadc5f1774da2252f155e334
Babbins <alimahmo@buffalo.edu>
2016-09-13 16:25:15 -0400

    Added cart model

--- server/db/index.js
+++ server/db/index.js
@@ -8,18 +8,19 @@ var Order = require('./models/order')
 var Character = require('./models/character');
 var Category = require('./models/category');
 var Review = require('./models/review');
+var Cart = require('./models/cart');
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
 
-// User.hasOne(Cart);
+User.hasOne(Cart);
 User.hasMany(Order, {as: 'orders'});
 User.hasMany(Review, {as: 'reviews'});
 
 Character.belongsToMany(Category, {through: 'CharacterCategory'});
 Character.hasMany(Review, {as: 'reviews'});
 Character.belongsToMany(Order, {through: 'CharacterOrder'});
-// Character.belongsToMany(Cart, {through: 'CharacterCart'});
+Character.belongsToMany(Cart, {through: 'CharacterCart'});
 Category.belongsToMany(Character, {through: 'CharacterCategory'});
 
 // if we had more models, we could associate them in this file
--- server/db/models/character.js
+++ server/db/models/character.js
@@ -1,3 +1,4 @@
+'use strict';
 var Sequelize = require('sequelize');
 var db = require('../_db');
 
