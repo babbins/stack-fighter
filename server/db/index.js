a6b22d0d3f72c6885d6c24bee41789f95642261a
Babbins <alimahmo@buffalo.edu>
2016-09-13 12:00:08 -0400

    added Character model and its relationships

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,11 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
-
+var Character = require('./models/character');
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
+
+Character.belongsToMany(Category, {through: 'CharacterCategory'});
+Character.hasMany(Review, {as: 'reviews'});
+Character.belongsToMany(Order, {through: 'CharacterOrder'});
+Character.belongsToMany(Cart, {through: 'CharacterCart'});
