528029f7340b20e9f7459d8198c1acf5f825a746
Nikita Petukhov <nedoumok99@gmail.com>
2016-09-13 10:57:27 -0400

    Made Order model, associated to characters

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,9 @@ module.exports = db;
 
 // eslint-disable-next-line no-unused-vars
 var User = require('./models/user');
+var Order = require('./models/order')
 
 // if we had more models, we could associate them in this file
 // e.g. User.hasMany(Reports)
+
+Order.belongsToMany(Character, { through: OrderCharacters})
