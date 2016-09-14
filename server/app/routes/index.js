24a2e2e68d4940bc43229ef56fc3b39497cd4d66
pimeson <pimeson@users.noreply.github.com>
2016-09-14 09:37:19 -0400

    Update index.js

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,12 @@
 var router = require('express').Router(); // eslint-disable-line new-cap
 module.exports = router;
 
-router.use('/members', require('./members'));
+ router.use('/members', require('./members'))
+ router.use('/characters', require('./characters'))
+ router.use('/orders', require('/orders'))		
+ router.use('/catgories', require('/catgories'))		
+ router.use('/reviews', require('/reviews'))		
+ router.use('/users', require('/users'))
 
 // Make sure this is after all of
 // the registered routes!
