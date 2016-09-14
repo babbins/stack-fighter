4bc0700e5a63d52d1a1f62093840af4635318373
Naohito Sadoshima <nsadoshima@gmail.com>
2016-09-14 17:12:10 -0400

    IT WORKS!

--- browser/js/character/characterSelect.ctrl.js
+++ browser/js/character/characterSelect.ctrl.js
@@ -1,3 +1,11 @@
 app.controller('CharacterSelectCtrl', function($scope, characters){
+  
   $scope.characters = characters;
+
+  $scope.selectedCharacter = characters[0];
+
+  $scope.selectCharacter = function(character){
+    $scope.selectedCharacter = character;
+  }
+
 });
