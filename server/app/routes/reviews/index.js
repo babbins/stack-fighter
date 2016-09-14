4c698920a8dc8d4d85e020a98f4a401ffd18f6fa
Nikita Petukhov <nedoumok99@gmail.com>
2016-09-14 12:14:59 -0400

    phatty

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -3,26 +3,22 @@ var router = require('express').Router(); // eslint-disable-line new-cap
 module.exports = router;
 var Review = require('../../../db/models/review')
 router.post('/', function(req, res, next){
-  Review.create(req.body).then(function(createdReview){
-    res.send(createdReview)
-  }).catch(next)
+  Review.create(req.body)
+  .then(createdReview => res.send(createdReview))
+  .catch(next)
 })
 
 router.put('/:id', function(req, res, next) {
-  Review.findById(req.params.id).then(function(foundReview){
-    return foundReview.update(req.body)
-  }).then(function(updatedReview){
-    res.send(updatedReview)
-  })
+  Review.findById(req.params.id)
+  .then(foundReview => foundReview.update(req.body))
+  .then(updatedReview => res.send(updatedReview))
   .catch(next)
 })
 
 router.delete('/:id', function(req, res, next) {
-  Review.findById(req.params.id).then(function(foundReview){
-    return foundReview.destroy()
-  }).then(function(){
-    res.send('destroyed')
-  })
+  Review.findById(req.params.id)
+  .then(foundReview => foundReview.destroy())
+  .then(() => res.send('destroyed'))
   .catch(next)
 })
 
@@ -32,16 +28,16 @@ router.get('/?', function(req, res, next) {
       where: {
         userId: req.query.user
       }
-    }).then(function(foundReviews){
-      res.send(foundReviews)
-    }).catch(next)
+    })
+    .then(foundReviews => res.send(foundReviews))
+    .catch(next)
   } else if (req.query.character) {
     Review.findAll({
       where: {
         characterId: req.query.character
       }
-    }).then(function(foundReviews){
-      res.send(foundReviews)
-    }).catch(next)
+    })
+    .then(foundReviews => res.send(foundReviews))
+    .catch(next)
   }
 })
