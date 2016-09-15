cf6d51e4fc9b90661a8b9883d24231d3134205b5
Naohito Sadoshima <nsadoshima@gmail.com>
2016-09-15 17:26:01 -0400

    WIP

--- browser/js/character/characterSelect.ctrl.js
+++ browser/js/character/characterSelect.ctrl.js
@@ -1,15 +1,62 @@
-app.controller('CharacterSelectCtrl', function($scope, characters){
-  
-  $scope.characters = characters;
+app.controller('CharacterSelectCtrl', function($scope, characters, CategoryFactory) {
 
-  $scope.selectedCharacter = characters[0];
+  CategoryFactory.fetchAll()
+    .then(function(data) {
+      $scope.categories = data;
+      return;
+    })
+    .then(function(waiting) {
+      console.log($scope.categories);
 
-  $scope.selectCharacter = function(character){
-    $scope.selectedCharacter = character;
-  };
+      $scope.activeFilter = [];
 
-  $scope.addToCart = function(quantity, character){
-    console.log("This Button was pressed yo!", quantity, character);
-  };
+      $scope.sortedCategories = CategoryFactory.separateTypes($scope.categories);
+
+      $scope.characters = characters;
+
+      $scope.selectedCharacter = characters[0];
+
+      $scope.selectCharacter = function(character) {
+        $scope.selectedCharacter = character;
+      };
+
+      $scope.addFilter = function(filter) {
+        if (filter[0] !== '{') {
+          return;
+        }
+        filter = JSON.parse(filter);
+        var added = false;
+        if ($scope.activeFilter.length === 0) {
+          $scope.activeFilter.push(filter);
+          added = true;
+        } else {
+          for (var i = 0; i < $scope.activeFilter.length; i++) {
+            if ($scope.activeFilter[i].type === filter.type) {
+              $scope.activeFilter.splice(i, 1);
+              $scope.activeFilter.push(filter);
+              added = true;
+            }
+          }
+        }
+        if (!added) {
+          $scope.activeFilter.push(filter);
+        }
+      };
+
+      $scope.clearFilter = function() {
+        $scope.activeFilter = [];
+        console.log("hello!", $scope.activeFilter);
+      };
+
+      $scope.addToCart = function(quantity, character) {
+        console.log("This Button was pressed yo!", quantity, character);
+        console.log($scope.activeFilter);
+      };
+
+      $scope.addFilters = function(){
+        console.log("Adding these filters! : ", $scope.activeFilter);
+      }
+
+    });
 
 });
