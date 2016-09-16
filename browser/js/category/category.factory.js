db1b48c14e3e7b25b461b929ef54500f9a277408
Naohito Sadoshima <nsadoshima@gmail.com>
2016-09-16 15:59:41 -0400

    fixed merge conflict

--- browser/js/category/category.factory.js
+++ browser/js/category/category.factory.js
@@ -1,28 +1,4 @@
 app.factory('CategoryFactory', function($http){
-<<<<<<< HEAD
-  return {
-    separate: function(array){
-       var sortedArr = [];
-       var arrKey = [];
-       for (var i = 0; i < array.length ; i++){
-           var key;
-           if (arrKey.indexOf(array[i].type) === -1){
-               arrKey.push(array[i].type);
-               key = arrKey.length - 1;
-               sortedArr.push([array[i].type]);
-           } else {
-               key = arrKey.indexOf(array[i].type);
-           }
-           sortedArr[key].push(array[i]);
-       }
-       return sortedArr;
-    },
-    getAll: function(){
-      return $http.get('/api/categories').then(res => res.data);
-    }
-  }
-});
-=======
 
     var CategoryFactory = {};
 
@@ -54,4 +30,3 @@ app.factory('CategoryFactory', function($http){
     return CategoryFactory;
 
 });
->>>>>>> master
